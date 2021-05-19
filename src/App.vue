3015baad5d9dbbdeabaa25db1af360d2d6477dc4
kaokei <kaokei.rst@gmail.com>
2021-05-19 23:38:56 +0800

    mod: 自己维护webcam-easy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-row type="flex" align="top">
+      <a-col flex="750px">
+        <WebCam @takePicture="takePicture"></WebCam>
+      </a-col>
+      <a-col flex="auto">
+        <a-upload :file-list="fileList" :before-upload="beforeUpload">
+          <a-button type="primary" shape="round" icon="upload" size="large"
+            >上传照片</a-button
+          >
+        </a-upload>
+      </a-col>
+    </a-row>
+    <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+      <img alt="example" style="width: 100%" :src="previewImage" />
+    </a-modal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebCam from "./components/WebCam.vue";
+
+function convertFileToImage(file) {}
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
+let __uid = -1;
+function uid() {
+  return __uid--;
+}
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    WebCam,
+  },
+  data() {
+    return {
+      fileList: [],
+      previewVisible: false,
+      previewImage: "",
+    };
+  },
+  methods: {
+    async handlePreview(file) {
+      if (!file.url && !file.preview) {
+        file.preview = await getBase64(file.originFileObj);
+      }
+      this.previewImage = file.url || file.preview;
+      this.previewVisible = true;
+    },
+    handleCancel() {
+      this.previewVisible = false;
+    },
+    handleRemove(file) {
+      console.log("handleRemove :>> ", file);
+      const index = this.fileList.indexOf(file);
+      const newFileList = this.fileList.slice();
+      newFileList.splice(index, 1);
+      this.fileList = newFileList;
+    },
+    beforeUpload(file) {
+      console.log("beforeUpload :>> ", file);
+      getBase64(file).then((url) => {
+        file.thumbUrl = url;
+        this.fileList = [...this.fileList, file];
+      });
+      console.log("this.fileList :>> ", this.fileList);
+      return false;
+    },
+    takePicture(image) {
+      console.log("take Picture :>> ", image);
+      this.fileList = [
+        ...this.fileList,
+        {
+          uid: uid(),
+          name: "take picture",
+          status: "done",
+          url: image,
+          thumbUrl: image,
+        },
+      ];
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333;
 }
 </style>
